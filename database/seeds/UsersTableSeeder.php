1b9f0f19388a7b94532e40e72e7f920eb76dc80e
Dave Samojlenko <dsamojlenko@gmail.com>
2017-06-13 16:56:31 -0400

    created a seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Dave Samojlenko',
+            'email' => 'dave.samojlenko@tbs-sct.gc.ca',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
