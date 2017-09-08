6f9bfc21b99cc434662bc7791df03f2260efdb51
Dave Samojlenko <dsamojlenko@gmail.com>
2017-09-08 11:27:45 -0400

    truncate first

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->truncate();
+        
         DB::table('users')->insert([
             'name' => 'Dave Samojlenko',
             'email' => 'dave.samojlenko@tbs-sct.gc.ca',
