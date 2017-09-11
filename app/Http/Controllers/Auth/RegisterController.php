73671014c16d7b81be1b0289fcdbb550e5dfd670
Dave Samojlenko <dsamojlenko@gmail.com>
2017-09-11 10:27:05 -0400

    redirect to trips

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/trips';
 
     /**
      * Create a new controller instance.
