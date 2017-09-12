8337d9d4b7b213abe70c70f5ebb7196dd7b35ed1
Dave Samojlenko <dsamojlenko@gmail.com>
2017-09-12 17:08:24 -0400

    better file field styling

--- resources/views/trips/people/create.blade.php
+++ resources/views/trips/people/create.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
+@push('scripts')
+
+<script>
+    var file = document.getElementById('file');
+    file.onchange = function() {
+        if(file.files.length > 0) {
+            document.getElementById('file-name').innerHTML = file.files[0].name;
+        }
+    }
+</script>
+
+@endpush
+
 @section('content')
     <div class="columns">
         @push('nav-menu')
@@ -14,11 +27,22 @@
             <form action="{{ route('trips.people.store', $trip) }}" method="POST" enctype="multipart/form-data">
                 {{ csrf_field() }}
 
-                <div class="field">
-                    <label class="label" for="image">{{ __('Photo') }}</label>
-                    <p class="control">
-                        <input type="file" name="image" id="image">
-                    </p>
+
+                <div class="file has-name">
+                    <label class="file-label">
+                        <input class="file-input" type="file" name="image" id="file">
+                        <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="fa fa-upload"></i>
+                            </span>
+                            <span class="file-label">
+                                Choose a photo...
+                            </span>
+                        </span>
+                        <span class="file-name" id="file-name">
+                            ...
+                        </span>
+                    </label>
                 </div>
 
                 @include('trips.people._form')
