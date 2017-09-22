e70dd19afbf1b50961ff44bec30285cd6dfef720
Dave Samojlenko <dsamojlenko@gmail.com>
2017-09-22 09:37:12 -0400

    generate and download at api

--- app/Http/Controllers/Api/DownloadTripPackageController.php
+++ app/Http/Controllers/Api/DownloadTripPackageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Trip;
+use App\TripPackage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
@@ -10,9 +11,20 @@ use Illuminate\Support\Facades\Storage;
 class DownloadTripPackageController extends Controller
 {
     protected $storage_path;
+    private $package;
+
+    public function __construct(TripPackage $package)
+    {
+        $this->package = $package;
+    }
 
     public function __invoke(Trip $trip)
     {
+        ini_set('max_execution_time', 60);
+
+        $package = $this->package->generate($trip);
+
+        /*
         $this->storage_path = 'public/packages/trips/' . $trip->id;
 
         if (!Storage::exists($this->storage_path . '/package.zip')) {
@@ -21,7 +33,8 @@ class DownloadTripPackageController extends Controller
                 'message' => 'There is no package available for this trip'
             ], 404);
         }
+        */
 
-        return response()->download(storage_path('app/' . $this->storage_path . '/package.zip'));
+        return response()->download($package);
     }
 }
