01cf1d81c31730041e1e6b08ab69347d2926df4d
Dave Samojlenko <dsamojlenko@gmail.com>
2018-05-02 16:04:23 -0400

    allow anyone to generate a package

--- resources/views/trips/index.blade.php
+++ resources/views/trips/index.blade.php
@@ -39,7 +39,7 @@
                                     {{ __('Manage Trip') }}
                                 </span>
                             </a>
-                            @can('manage-system')
+
                                 <form class="is-inline download-form" action="{{ route('trips.generate', $trip) }}" method="POST">
                                     {{ csrf_field() }}
                                     <button type="submit" class="button download-button">
@@ -51,7 +51,7 @@
                                         </span>
                                     </button>
                                 </form>
-                            @endcan
+                            
                         </td>
                     </tr>
                 @endforeach
